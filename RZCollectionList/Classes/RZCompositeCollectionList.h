a2909970ab563da08c99fa17f430a8975f6e3840
Connor Smith <connor.smith@raizlabs.com>
2014-04-14 13:13:05 -0400

    finished documentation for RZCompositeCollectionList.h

--- RZCollectionList/Classes/RZCompositeCollectionList.h
+++ RZCollectionList/Classes/RZCompositeCollectionList.h
@@ -22,9 +22,37 @@
  */
 @property (nonatomic, readonly, copy) NSArray *sourceLists;
 
+/**
+ *  Initializer for an instance of RZCompositeCollectionList. You must provide one or more @b initialized source id<RZCollectionList> instances. 
+ *  For example, you may have an RZFetchedCollectionList and an RZArrayCollectionList that you want to combine as the data source for a UICollectionView.
+ *
+ *  @note BOOL ignoresSections will default to NO with this initializer. Use - (id)initWithSourceLists:(NSArray*)sourceLists ignoreSections:(BOOL)ignoreSections, passing YES, if you wish to initialize this list as one section.
+ *
+ *  @param sourceLists The id<RZCollectionList> lists that will comprise the returned instance of RZCompositeCollectionList.
+ *
+ *  @return An instance of RZCompositeCollectionList.
+ */
 - (id)initWithSourceLists:(NSArray*)sourceLists;
+
+/**
+ *  Initializer for an instance of RZCompositeCollectionList. You must provide one or more @b initialized source id<RZCollectionList> instances.
+ *  For example, you may have an RZFetchedCollectionList and an RZArrayCollectionList that you want to combine as the data source for a UICollectionView.
+ *  You also must provide a Boolean to represent whether you want this list to use the sections of the source lists, or to have all the data in a single section.
+ *
+ *  @param sourceLists    The id<RZCollectionList> lists that will comprise the returned instance of RZCompositeCollectionList.
+ *  @param ignoreSections YES treat all items as if they are in one section, NO will maintain the original sections of the source lists. Specifying NO for ignoreSections will not affect the sections of the source lists already in place - The RZCompositeCollectionList will just ignore them.
+ *
+ *  @return An instance of RZCompositeCollectionList.
+ */
 - (id)initWithSourceLists:(NSArray*)sourceLists ignoreSections:(BOOL)ignoreSections; // Default is NO. If YES, will flatten list into one section.
 
+/**
+ *  Returns the list at the specified section in the RZCompositeCollectionList instance. This will work even if _ignoreSections is set to YES.
+ *
+ *  @param sectionIndex The index of the section within the instance of RZCompositeCollectionList.
+ *
+ *  @return One of the source lists of this instance of RZCompositeCollectionList or nil if none exist at that index.
+ */
 - (id<RZCollectionList>)sourceListForSectionIndex:(NSUInteger)sectionIndex;
 
 @end
